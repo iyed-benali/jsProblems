d88d413928a71f65d780155ba36317be28c80b2e
iyed-benali <113175835+iyed-benali@users.noreply.github.com>
2024-07-26 12:33:56 +0100

    Create domManupiation.js

---
+++ domManupiation.js
@@ -0,0 +1,12 @@
+Create an HTML file with a <div> element that contains some text. Write a JavaScript function changeText that changes the text content of the <div> element to "Hello, World!" when a button is clicked.
+
+
+  Create an HTML file with a <button> element. Write a JavaScript function changeBackgroundColor that changes the background color of the <body> element to lightblue when the button is clicked.
+
+
+  Create an HTML file with a <div> element and a <button> element. Write a JavaScript function toggleVisibility that shows or hides the <div> element each time the button is clicked.
+
+  Create an HTML file with an unordered list (<ul>) and a <button> element. Write a JavaScript function addListItem that adds a new list item (<li>) with the text "New Item" to the list each time the button is clicked
+
+
+Create an HTML file with an <img> element and a <button> element. Write a JavaScript function changeImage that changes the source of the image to a different URL when the button is clicked.
